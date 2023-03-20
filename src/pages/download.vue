08d672fdf532d7f38841451adf4da214c7ca64dc
Vinodh Rajan <vinodh@virtualvinodh.com>
2023-03-20 21:43:53 +0100

    fixing Keyman installation guideline and confusables and conjunct list

--- src/pages/download.vue
+++ src/pages/download.vue
@@ -17,10 +17,9 @@
     <div class="q-display-1 q-mt-lg q-mb-md">Offline Keyboard</div>
     <div class="q-body-1">There is a Keyman keyboard available for any one who would like to type Grantha directly into their word processors. Keyman is a popularly used software to support input in different scripts.</div>
     <div class="q-ma-md"><img src="../statics/word-grantha-demo.png" width="70%" /> </div>
-    <div class="q-body-1"><a href="https://github.com/virtualvinodh/grantha-keyman-keyboards/raw/main/-grantha_unicode.exe?raw=true">Integrated Keyman
-    Keyboard (if you don't have Keyman installed in your system)</a></div>
-    <div class="q-body-1"><a href="https://github.com/virtualvinodh/grantha-keyman-keyboards/raw/main/grantha_unicode.kmp"> Keyman Package (if Keyman is already installed)</a></div>
-     <div class="q-body-1 q-mt-md">Double-clicking on the downloaded .exe file might result in a dialogue box that asks for your confirmation to proceed with the installation. Please select 'Yes' </div> <br/>
+    <div class="q-body-1">Please download and install <a href="https://keyman.com/windows/">Keyman</a> from the official site and then proceed to install the Grantha keyboard package linked below.</div><br/>
+    <div class="q-body-1"><a href="https://github.com/virtualvinodh/grantha-keyman-keyboards/raw/main/grantha_unicode.kmp"> Keyman Keyboard Package</a></div>
+     <div class="q-body-1 q-mt-md">Double-clicking on the Keyman installer might result in a dialogue box that asks for your confirmation to proceed with the installation. Please select 'Yes' </div> <br/>
     <div class="q-body-1">You can read the instructions for using the keyboard <a href="https://github.com/virtualvinodh/grantha-keyman-keyboards/blob/main/src/how_to_use.pdf" target="_blank">here</a>.</div>
     <div class="q-display-1 q-mt-lg q-mb-md">App</div>
     <div class="q-body-1">Android app coming soon...</div>
