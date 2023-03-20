479a4d226ee23a8c8d178697173f4bd55b60519c
Vinodh Rajan <vinodh@virtualvinodh.com>
2023-03-20 22:10:36 +0100

    updating keyman keyoard instructions

--- src/pages/download.vue
+++ src/pages/download.vue
@@ -17,7 +17,7 @@
     <div class="q-display-1 q-mt-lg q-mb-md">Offline Keyboard</div>
     <div class="q-body-1">There is a Keyman keyboard available for any one who would like to type Grantha directly into their word processors. Keyman is a popularly used software to support input in different scripts.</div>
     <div class="q-ma-md"><img src="../statics/word-grantha-demo.png" width="70%" /> </div>
-    <div class="q-body-1">Please download and install <a href="https://keyman.com/windows/">Keyman</a> from the official site and then proceed to install the Grantha keyboard package linked below.</div><br/>
+    <div class="q-body-1">If you do not have Keyman already installed, please download and install <a href="https://keyman.com/windows/">Keyman</a> from the official site. You can either select Keyman with a pre-existing language of your choice or scroll down the page to select <i>Keyman for Windows without any keyboards</i>. Once Keyman has been insalled, please download and install the Grantha keyboard package linked below.</div><br/>
     <div class="q-body-1"><a href="https://github.com/virtualvinodh/grantha-keyman-keyboards/raw/main/grantha_unicode.kmp"> Keyman Keyboard Package</a></div>
      <div class="q-body-1 q-mt-md">Double-clicking on the Keyman installer might result in a dialogue box that asks for your confirmation to proceed with the installation. Please select 'Yes' </div> <br/>
     <div class="q-body-1">You can read the instructions for using the keyboard <a href="https://github.com/virtualvinodh/grantha-keyman-keyboards/blob/main/src/how_to_use.pdf" target="_blank">here</a>.</div>
